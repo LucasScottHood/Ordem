7d0c9bb8e2cd05711ef8bc9830faae11cef979cb
LucasScottHood <lscotthood@gmail.com>
2026-08-08 16:23:15 -0300

    Add files via upload

--- BancoJuventude/Golpe.js
+++ BancoJuventude/Golpe.js
@@ -1,3 +1,10 @@
+import {Logado} from "./Login.js";
+
+if (!Logado) {
+	alert("Você precisa estar logado para acessar esta página!");
+	window.location.href = "Login.html";
+}
+
 let Cartas = [ ["Ás de Paus", "2 de Paus", "3 de Paus", "4 de Paus", "5 de Paus", "6 de Paus", "7 de Paus", "Dama de Paus", "Valete de Paus", "Rei de Paus"], ["Ás de Copas", "2 de Copas", "3 de Copas", "4 de Copas", "5 de Copas", "6 de Copas", "7 de Copas", "Dama de Copas", "Valete de Copas", "Rei de Copas"], ["Ás de Espadas", "2 de Espadas", "3 de Espadas", "4 de Espadas", "5 de Espadas", "6 de Espadas", "7 de Espadas", "Dama de Espadas", "Valete de Espadas", "Rei de Espadas"], ["Ás de Ouros", "2 de Ouros", "3 de Ouros", "4 de Ouros", "5 de Ouros", "6 de Ouros", "7 de Ouros", "Dama de Ouros", "Valete de Ouros", "Rei de Ouros"] ];
 let j = 0, CartaUm = 0, CartaDois = 0, CartaTres = 0, CartasNoJogo = [];
 while (j < (Jogadores+1)*3)
--- BancoJuventude/Login.js
+++ BancoJuventude/Login.js
@@ -2,6 +2,7 @@ const Formulario = document.getElementById("Formulario");
 const Usuario = document.getElementById("email");
 const Senha = document.getElementById("password");
 const Mensagem = document.getElementById("mensagem");
+export let Logado = false;
 
 const UsuariosRegistrados = [
     { email: "igorpazzianoto@gmail.com", senha: "123456"},
@@ -23,6 +24,7 @@ const ProcurarUsuario = UsuariosNoBancoDeDados.find(usuario =>
 if (ProcurarUsuario) {
         alert("Login realizado com sucesso!");
         window.location.href = "BancoJuventude.html";
+        Logado = true;
     } else {
         Mensagem.textContent = "E-mail ou senha incorretos!";
     }
